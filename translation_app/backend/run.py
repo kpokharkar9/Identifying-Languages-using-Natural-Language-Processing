f6c7367a7cd5ee6aef661524e0dedab2e5dfc077
Kasturi Pokharkar <kasturipokharkar11@gmail.com>
2024-06-20 21:58:37 -0700

    Imported rest framwork and added route

--- translation_app/backend/run.py
+++ translation_app/backend/run.py
@@ -1,6 +1,14 @@
-from app import app
+from app.routes import setup_routes
 import logging
-import os
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+setup_routes(api)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
