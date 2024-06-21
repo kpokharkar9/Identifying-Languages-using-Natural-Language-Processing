567ced1b038775bd04d68423c51840bb5c648d07
Kasturi Pokharkar <kasturipokharkar11@gmail.com>
2024-06-20 17:23:04 -0700

    Configure logging for the Flask Application

--- translation_app/run.py
+++ translation_app/run.py
@@ -1,4 +1,11 @@
 from app import app
+import logging
+import os
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
+    logger.info("Starting Flask application")
     app.run(debug=True)
